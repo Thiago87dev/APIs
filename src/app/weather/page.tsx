3fb4408a2cd60eeeeb48256a742d232298933165
thiago alves <thiago87gsa@gmail.com>
2024-03-20 16:42:07 -0300

    css weather

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface WeatherProps {
   name: string;
@@ -20,10 +20,19 @@ interface WeatherProps {
   };
 }
 
+interface DateTimeProps {
+  formattedDate: string;
+  formattedTime: string;
+}
+
 const Weather = () => {
   const [city, setCity] = useState("");
   const [weather, setWeather] = useState<WeatherProps | null>(null);
   const [error, setError] = useState(false);
+  const [dateTime, setDateTime] = useState<DateTimeProps>({
+    formattedDate: new Date().toLocaleDateString(),
+    formattedTime: new Date().toLocaleTimeString(),
+  });
 
   let kmSpeed: number | undefined;
 
@@ -55,6 +64,23 @@ const Weather = () => {
     kmSpeed = Math.round(weather?.wind.speed * 3.6);
   }
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const now = new Date();
+      const options: Intl.DateTimeFormatOptions = {  day: 'numeric', month: 'short', year: 'numeric', hour: 'numeric', minute: 'numeric' };
+      
+      // Obtendo o dia por extenso e a data formatada
+      const formattedDate = now.toLocaleDateString('pt-BR', options);
+  
+      // Obtendo a hora sem os segundos
+      const formattedTime = now.toLocaleTimeString('pt-BR', { hour: 'numeric', minute: 'numeric' });
+  
+      setDateTime({ formattedDate, formattedTime });
+    }, 1000);
+  
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col justify-center items-center gap-4 p-10">
@@ -68,48 +94,57 @@ const Weather = () => {
           pesquisar
         </button>
       </div>
-      {error && (
-        <div>Cidade não encontrada</div>
-      )}
+      {error && <div>Cidade não encontrada</div>}
       {weather && !error && (
-        <div className="flex flex-col justify-center text-black items-center gap-4 w-96 p-10 rounded-xl bg-white">
-          <div className="flex gap-2 w-full justify-center ">
-            <p>Local:</p>
-            {weather?.name}
-          </div>
-          <div className="flex gap-2">
-            <p>Temperatura:</p>
-            {weather?.main.temp !== undefined && (
-              <div>{Math.round(weather?.main.temp)}ºC</div>
-            )}
-          </div>
-          <div className="flex gap-2 w-full justify-center">
-            <p>Sensação termica:</p>
-            {weather?.main.temp !== undefined && (
-              <div>{Math.round(weather?.main.feels_like)}ºC</div>
-            )}
-          </div>
-          <div className="flex gap-2 w-full items-center  justify-center">
-            <p>Humidade:</p>
-            {weather?.main.humidity}%
-          </div>
+        <div className="flex flex-col justify-center text-black items-center gap-4 w-[35rem] p-5 rounded-xl bg-white">
           <div className="flex gap-2 flex-col items-center w-full ">
-            <p>Tempo:</p>
             {weather.weather.map((item) => (
-              <div className="flex gap-2 items-center flex-col " key={item.id}>
-                {item.description}
-                <Image
-                  alt={item.description}
-                  width={60}
-                  height={60}
-                  src={`https://openweathermap.org/img/wn/${item.icon}.png`}
-                />
+              <div className="flex flex-col gap-2  w-full" key={item.id}>
+                <div className="flex items-center ">
+                  <div className="flex text-6xl relative">
+                    {weather?.main.temp !== undefined && (
+                      <div>
+                        <span>{Math.round(weather?.main.temp)}</span>
+                        <span className="text-base absolute top-0 right-[-1]">
+                          ºC
+                        </span>
+                      </div>
+                    )}
+                  </div>
+                  <Image
+                    alt={item.description}
+                    width={70}
+                    height={70}
+                    src={`https://openweathermap.org/img/wn/${item.icon}.png`}
+                  />
+                </div>
+
+                <div className="flex  justify-between ">
+                  <div className="flex flex-col justify-center ">
+                    <div className="flex gap-1">
+                      <p>Sensação: </p>
+                      {weather?.main.temp !== undefined && (
+                        <div>{Math.round(weather?.main.feels_like)}ºC</div>
+                      )}
+                    </div>
+                    <div className="flex gap-1">
+                      <p>Humidade:</p>
+                      {weather?.main.humidity}%
+                    </div>
+                    <div className="flex gap-1 ">
+                      <p>Vento:</p>
+                      {kmSpeed} Km/h
+                    </div>
+                  </div>
+                  <div className="flex flex-col items-end">
+                    <div className="text-2xl font-bold">{weather?.name}</div>
+                    {dateTime.formattedDate}
+                    
+                    <div>{item.description}</div>
+                  </div>
+                </div>
               </div>
             ))}
-          </div>
-          <div className="flex gap-2 w-full justify-center ">
-            <p>Velocidade do vento:</p>
-            {kmSpeed} Km/h
           </div>
         </div>
       )}
